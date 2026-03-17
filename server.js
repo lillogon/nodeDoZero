2f466d5f443bdab5e24820e793fc8dd72dd36713
Murillo Gonçalves <lillogon@outlook.com>
2026-03-16 22:14:20 -0300

    Aprendendo nodejs com bd em memória

---
+++ server.js
@@ -0,0 +1,60 @@
+//import { createServer} from 'node:http'
+
+//const server = createServer((request, response) => {
+//    response.write('Hello word!')
+
+//    return response.end()
+//})
+
+//server.listen(3333)
+
+import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
+
+const server = fastify()
+
+const database = new DatabaseMemory()
+
+server.post('/videos', (request, reply) => {
+    const {title, description, duration} = request.body
+
+    database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', () => {
+    const videos = database.list()
+
+    return videos
+})
+
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const {title, description, duration} = request.body
+
+
+    database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+server.listen({
+    port: 3333,
+})
